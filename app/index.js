86fd5994271f2abcb47465abe810e9caa6ff08bf
Tracy Wei <tracywei27@gmail.com>
2023-11-20 11:44:27 -0600

    rough layout and style of home

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, SafeAreaView, View, Image, TextInput } from "react-native";
+import { Text, SafeAreaView, View, Image, TextInput, ImageBackground } from "react-native";
 import { AppStyles } from "../styles";
 
 export default function Home() {
@@ -11,21 +11,34 @@ export default function Home() {
   }
 
   return (
+    <ImageBackground source={require("../assets/background.png")} opacity='0.5' style={styles.backgroundImage}>
     <SafeAreaView>
-      <Text style={styles.title}>Inter Bold</Text>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>Button Text</Text>
-      </View>
-      <Image
-        source={require("../assets/people/profileImage.jpg")}
-        style={styles.profileImage}
-      />
-      <TextInput
-        style={styles.textInputBox}
-        onChangeText={onChangeText}
-        value={text}
-        placeholder="Filler Text"
-      />
+        <Text style={styles.timeLeftText}>01h 03 m</Text>
+        <Text style={styles.timeSubheadingText}>until sent to</Text>
+        <View style={styles.container}> 
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Change time</Text>
+          </View>
+          <Image
+            source={require("../assets/people/profileImage.jpg")}
+            style={styles.profileImage}
+          />
+        </View>
+        <Text style={styles.personNameText}>Greg</Text>
+        <View style={styles.container}> 
+          <Image
+            source={require("../assets/graphics/EmptyBottle.png")}
+            style={styles.bottleImage}
+          />
+        </View>
+        
+        {/* <TextInput
+          style={styles.textInputBox}
+          onChangeText={onChangeText}
+          value={text}
+          placeholder="Filler Text"
+        /> */}
     </SafeAreaView>
+    </ImageBackground>
   );
 }
--- styles.js
+++ styles.js
@@ -18,7 +18,7 @@ const { height: windowHeight, width: windowWidth } = Dimensions.get("window");
 export const AppStyles = () => {
   const [fontsLoaded] = useFonts({
     'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
-    'Inter-Regular': require('./assets/fonts/Inter-Bold.ttf'),
+    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
   });
 
   if (!fontsLoaded) {
@@ -43,10 +43,15 @@ export const AppStyles = () => {
 
   return StyleSheet.create({
     container: {
+      // flex: 1,
+      alignItems: 'center',
+    },
+    backgroundImage: {
       flex: 1,
       justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: theme.primaryBackground,
+      resizeMode: 'cover',
+      width: '100%',
+      height: '100%',
     },
     text: {
       fontSize: 16,
@@ -65,6 +70,8 @@ export const AppStyles = () => {
       borderRadius: 17,
       backgroundColor: theme.buttonColor,
       justifyContent: "center",
+      marginTop: 10,
+      marginBottom: 10,
     },
     buttonText: {
       fontSize: 18,
@@ -79,6 +86,10 @@ export const AppStyles = () => {
       borderWidth: 3,
       borderColor: theme.secondaryColor,
     },
+    bottleImage: {
+      width: windowWidth*0.55,
+      height: windowHeight*0.5,
+    },
     textInputBox: {
       borderRadius: 20,
       borderColor: theme.borderOutlineColor,
@@ -88,6 +99,25 @@ export const AppStyles = () => {
       margin: 12,
       borderWidth: 1,
       padding: 10,
+    },
+    timeLeftText: {
+      color: theme.primaryColor,
+      textAlign: "center",
+      fontFamily: theme.titleFont,
+      fontSize: 40,
+      fontWeight: 500,
+    },
+    timeSubheadingText: {
+      color: theme.primaryColor,
+      textAlign: "center",
+      fontFamily: theme.textFont,
+      fontSize: 30,
+    },
+    personNameText: {
+      color: theme.primaryColor,
+      textAlign: "center",
+      fontFamily: theme.titleFont,
+      fontSize: 30,
     },
   });
 };
