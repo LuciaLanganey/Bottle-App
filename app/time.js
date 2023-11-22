bd02bc57825350569652e178962f2f3ec84c5486
Tracy Wei <tracywei27@gmail.com>
2023-11-22 15:11:48 -0600

    added time picker but uninstalled & unimplemented

--- app/time.js
+++ app/time.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { Text, SafeAreaView, View, Image, ImageBackground } from "react-native";
 import { AppStyles } from "../styles";
+import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { Icon } from 'react-native-elements'
 
 export default function Time() {
   const styles = AppStyles();
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const handleConfirm = (date) => {
+    console.warn("A date has been picked: ", date);
+    hideDatePicker();
+  };
 
   if (!styles) {
     return null;
@@ -23,6 +38,18 @@ export default function Time() {
           </View>
         </View>
         <Text style={styles.personNameText}>Greg</Text>
+        <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'center'}}>
+          <Text style={styles.timeLeftText}>11:00 PM </Text>
+            <Icon name='clock-edit' type='material-community' color='#23AFBB' size={40}
+              style={{alignSelf: 'center', marginTop: 50, marginLeft: 10}} 
+              onPress={() => showDatePicker} /> 
+        </View>
+        <DateTimePickerModal
+          isVisible={isDatePickerVisible}
+          mode="time"
+          onConfirm={handleConfirm}
+          onCancel={hideDatePicker}
+        />
     </SafeAreaView>
     </ImageBackground>
   );
