63d3c90d51deaaac107b3060280bc4f859985ac5
LuciaLanganey <langaneylucia03@gmail.com>
2023-11-27 21:04:35 -0800

    made reciever popup, began implementing search bar

--- app/index.js
+++ app/index.js
@@ -1,18 +1,35 @@
 import React, { useState, useEffect } from "react";
-import { Text, SafeAreaView, View, Image, TextInput, ImageBackground, TouchableOpacity } from "react-native";
+import {
+  Text,
+  SafeAreaView,
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+  Modal,
+  Pressable,
+  TextInput,
+  Button,
+  FlatList,
+} from "react-native";
 import { AppStyles } from "../styles";
-import { Icon } from 'react-native-elements'
-import { useNavigation } from '@react-navigation/native';
+import { Icon } from "react-native-elements";
+import { FontAwesome } from "@expo/vector-icons";
+
+import { useNavigation } from "@react-navigation/native";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+// import { TextInput } from "react-native-gesture-handler";
 
 export default function Home() {
   const styles = AppStyles();
   // const [text, onChangeText] = React.useState('');
   const navigation = useNavigation();
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
-  const [selectedTime, setSelectedTime] = useState('11:00 AM');
-  const [timeLeft, setTimeLeft] = useState('00h 00m');
+  const [selectedTime, setSelectedTime] = useState("11:00 AM");
+  const [timeLeft, setTimeLeft] = useState("00h 00m");
 
+  // time picker functions
   const showDatePicker = () => {
     setDatePickerVisibility(true);
   };
@@ -24,20 +41,22 @@ export default function Home() {
   const handleConfirm = (date) => {
     let selectedHour = date.getHours();
     const selectedMinute = date.getMinutes();
-    let amPm = 'AM';
-  
+    let amPm = "AM";
+
     if (selectedHour >= 12) {
-      amPm = 'PM';
+      amPm = "PM";
       if (selectedHour > 12) {
         selectedHour -= 12;
       }
     }
-  
+
     if (selectedHour === 0) {
       selectedHour = 12;
     }
-  
-    const formattedTime = `${selectedHour}:${selectedMinute < 10 ? '0' + selectedMinute : selectedMinute} ${amPm}`;
+
+    const formattedTime = `${selectedHour}:${
+      selectedMinute < 10 ? "0" + selectedMinute : selectedMinute
+    } ${amPm}`;
     setSelectedTime(formattedTime);
     hideDatePicker();
   };
@@ -46,7 +65,9 @@ export default function Home() {
     const interval = setInterval(() => {
       const now = new Date();
       const selected = new Date();
-      const [hours, minutes] = selectedTime.split(':').map((str) => parseInt(str));
+      const [hours, minutes] = selectedTime
+        .split(":")
+        .map((str) => parseInt(str));
       selected.setHours(hours);
       selected.setMinutes(minutes);
 
@@ -58,87 +79,267 @@ export default function Home() {
       }
 
       const hoursLeft = Math.floor(difference / (1000 * 60 * 60));
-      const minutesLeft = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+      const minutesLeft = Math.floor(
+        (difference % (1000 * 60 * 60)) / (1000 * 60)
+      );
 
-      const formattedTimeLeft = `${hoursLeft.toString().padStart(2, '0')}h ${minutesLeft.toString().padStart(2, '0')}m`;
+      const formattedTimeLeft = `${hoursLeft
+        .toString()
+        .padStart(2, "0")}h ${minutesLeft.toString().padStart(2, "0")}m`;
       setTimeLeft(formattedTimeLeft);
     }, 1000);
 
     return () => clearInterval(interval);
   }, [selectedTime]);
 
+  // change reciever functions
+  const [modalVisible, setModalVisible] = useState(false);
+
+  // search bar
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = () => {
+    // In a real application, you would perform a search using the searchQuery
+    // and update the searchResults state with the actual search results.
+    // For simplicity, we'll just simulate some search results here.
+    const simulatedResults = [
+      { id: "1", name: "Result 1" },
+      { id: "2", name: "Result 2" },
+      { id: "3", name: "Result 3" },
+    ];
+
+    setSearchResults(simulatedResults);
+  };
 
   if (!styles) {
     return null;
   }
 
   return (
-    <ImageBackground source={require("../assets/background.png")} opacity='0.5' style={styles.backgroundImage}>
-    <SafeAreaView>
-        <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'center'}}>
+    <ImageBackground
+      source={require("../assets/background.png")}
+      opacity="0.5"
+      style={styles.backgroundImage}
+    >
+      <SafeAreaView>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <Text style={styles.timeLeftText}>{timeLeft}</Text>
-            <Icon name='clock-edit' type='material-community' color='#23AFBB' size={40}
-              style={{alignSelf: 'center', marginTop: 50, marginLeft: 10}} 
-              onPress={() => showDatePicker()} /> 
+          <Icon
+            name="clock-edit"
+            type="material-community"
+            color="#23AFBB"
+            size={40}
+            style={{ alignSelf: "center", marginTop: 50, marginLeft: 10 }}
+            onPress={() => showDatePicker()}
+          />
         </View>
-        <Text style={styles.timeSubheadingText}>until sent</Text>
-        <View style={styles.container}> 
-          <View style={{alignItems: 'center'}}>
+        <Text style={styles.timeSubheadingText}>until sent to</Text>
+        <View style={styles.container}>
+          {/* Change Reciever Pop-up */}
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            {/* Modal Content */}
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <View style={styles.closeIconContainer}>
+                  <Icon
+                    name="close"
+                    type="ionicons"
+                    color="#23AFBB"
+                    size={30}
+                    onPress={() => setModalVisible(!modalVisible)}
+                  />
+                </View>
+                <Text style={styles.modalText}>Current reciever:</Text>
+                {/* Current Reciever Pic and Name*/}
+                <View style={{ alignItems: "center" }}>
+                  <Image
+                    source={require("../assets/people/profileImage.jpg")}
+                    style={styles.modalRecieverImage}
+                  />
+                  <Text style={styles.modalRecieverName}>Greg</Text>
+                </View>
+
+                <Text style={styles.modalText}>Select a new reciever:</Text>
+                {/* Search Bar */}
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <Icon
+                    name="search"
+                    type="font-awesome"
+                    color="#23AFBB"
+                    size={30}
+                    style={{ marginRight: 10 }}
+                    onPress={() => handleSearch()}
+                  />
+                  <TextInput
+                    style={styles.searchBarContainer}
+                    placeholder="Search friends"
+                    value={searchQuery}
+                    onChangeText={(text) => setSearchQuery(text)}
+                  />
+                </View>
+
+                {/* Scrollable list of all friends */}
+                {searchResults.length > 0 && (
+                  <FlatList
+                    data={searchResults}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => (
+                      <View style={{ marginTop: 10 }}>
+                        <Text>{item.name}</Text>
+                      </View>
+                    )}
+                  />
+                )}
+
+                <Pressable
+                  style={[styles.button]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Done</Text>
+                </Pressable>
+              </View>
+            </View>
+          </Modal>
+
+          <View style={{ alignItems: "center" }}>
             <Image
               source={require("../assets/people/profileImage.jpg")}
               style={styles.profileImage}
             />
           </View>
         </View>
-        <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'center'}}>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <Text style={styles.personNameText}>Greg</Text>
-          <Icon name='account-edit' type='material-community' color='#23AFBB' size={50} style={{marginLeft: 'auto'}} 
-            onPress={() => console.log('hello')} />
+          <Icon
+            name="account-edit"
+            type="material-community"
+            color="#23AFBB"
+            size={50}
+            style={{ marginLeft: "auto" }}
+            onPress={() => setModalVisible(true)}
+          />
         </View>
-           
-        <View style={styles.container}> 
+
+        <View style={styles.container}>
           <Image
             source={require("../assets/graphics/bottle-cropped.png")}
             style={styles.bottleImage}
           />
         </View>
-        <View style={{justifyContent:'center',alignItems:'center',alignSelf:'center', position:'absolute', bottom: '13%'}}>
-          <Icon name='add-circle' type='ionicons' color='#23AFBB' size={100} onPress={() => console.log('hello')} />
+        <View
+          style={{
+            justifyContent: "center",
+            alignItems: "center",
+            alignSelf: "center",
+            position: "absolute",
+            bottom: "13%",
+          }}
+        >
+          <Icon
+            name="add-circle"
+            type="ionicons"
+            color="#23AFBB"
+            size={100}
+            onPress={() => console.log("hello")}
+          />
           {/* NOTE: add button is here - change link to next screen with onPress */}
         </View>
 
-
-        {/* <TextInput
-          style={styles.textInputBox}
-          onChangeText={onChangeText}
-          value={text}
-          placeholder="Filler Text"
-        /> */}
         <DateTimePickerModal
           isVisible={isDatePickerVisible}
           mode="time"
           onConfirm={handleConfirm}
           onCancel={hideDatePicker}
-          textColor='#186174'
+          textColor="#186174"
           customHeaderIOS={(headerProps) => (
-            <View style={{ justifyContent: 'space-between', alignItems: 'center', paddingVertical: 10, paddingHorizontal: 20 }}>
-              <Text style={{ color: '#186174', fontSize: 18, marginTop: 20, marginBottom: 10 }}>Change your bottle delivery time</Text>
-              <Text style={{ color: '#186174', fontSize: 24, fontWeight: 700 }}>{selectedTime}</Text>
-              <Text style={{ color: '#186174', fontSize: 18, marginTop: 10}}>to:</Text>
+            <View
+              style={{
+                justifyContent: "space-between",
+                alignItems: "center",
+                paddingVertical: 10,
+                paddingHorizontal: 20,
+              }}
+            >
+              <Text
+                style={{
+                  color: "#186174",
+                  fontSize: 18,
+                  marginTop: 20,
+                  marginBottom: 10,
+                }}
+              >
+                Change your bottle delivery time
+              </Text>
+              <Text style={{ color: "#186174", fontSize: 24, fontWeight: 700 }}>
+                {selectedTime}
+              </Text>
+              <Text style={{ color: "#186174", fontSize: 18, marginTop: 10 }}>
+                to:
+              </Text>
             </View>
           )}
           customCancelButtonIOS={(cancelProps) => (
-            <TouchableOpacity onPress={cancelProps.onPress } style={{backgroundColor: 'white', alignItems: 'center', borderRadius: 15}}>
-              <Text style={{ color: '#23AFBB', fontSize: 20, paddingHorizontal: 20, paddingVertical: 15 }}>Cancel</Text>
+            <TouchableOpacity
+              onPress={cancelProps.onPress}
+              style={{
+                backgroundColor: "white",
+                alignItems: "center",
+                borderRadius: 15,
+              }}
+            >
+              <Text
+                style={{
+                  color: "#23AFBB",
+                  fontSize: 20,
+                  paddingHorizontal: 20,
+                  paddingVertical: 15,
+                }}
+              >
+                Cancel
+              </Text>
             </TouchableOpacity>
           )}
           customConfirmButtonIOS={(confirmProps) => (
-            <TouchableOpacity onPress={confirmProps.onPress}  style={{backgroundColor: '#23AFBB', alignItems: 'center'}}>
-              <Text style={{ color: 'white', fontSize: 20, paddingHorizontal: 20, paddingVertical: 15, fontWeight: 600}}>Confirm</Text>
+            <TouchableOpacity
+              onPress={confirmProps.onPress}
+              style={{ backgroundColor: "#23AFBB", alignItems: "center" }}
+            >
+              <Text
+                style={{
+                  color: "white",
+                  fontSize: 20,
+                  paddingHorizontal: 20,
+                  paddingVertical: 15,
+                  fontWeight: 600,
+                }}
+              >
+                Confirm
+              </Text>
             </TouchableOpacity>
           )}
         />
-    </SafeAreaView>
+      </SafeAreaView>
     </ImageBackground>
   );
 }
--- styles.js
+++ styles.js
@@ -1,32 +1,32 @@
-import { StyleSheet, Dimensions } from 'react-native';
-import { useFonts } from 'expo-font';
+import { StyleSheet, Dimensions } from "react-native";
+import { useFonts } from "expo-font";
 
 const theme = {
-  backgroundColor: '#ffffff',
-  primaryColor: '#186174',
-  secondaryColor: '#23AFBB',
-  borderOutlineColor: '#D9D9D9',
-  buttonColor: '#23AFBB',
-  white: '#FFFFFF',
-  black: '#000000',
-  titleFont: 'Inter-Bold',
-  textFont: 'Inter-Regular',
+  backgroundColor: "#ffffff",
+  primaryColor: "#186174",
+  secondaryColor: "#23AFBB",
+  borderOutlineColor: "#D9D9D9",
+  buttonColor: "#23AFBB",
+  white: "#FFFFFF",
+  black: "#000000",
+  titleFont: "Inter-Bold",
+  textFont: "Inter-Regular",
 };
 
 const { height: windowHeight, width: windowWidth } = Dimensions.get("window");
 
 export const AppStyles = () => {
   const [fontsLoaded] = useFonts({
-    'Inter-Bold': require('./assets/fonts/Inter-Bold.ttf'),
-    'Inter-Regular': require('./assets/fonts/Inter-Regular.ttf'),
+    "Inter-Bold": require("./assets/fonts/Inter-Bold.ttf"),
+    "Inter-Regular": require("./assets/fonts/Inter-Regular.ttf"),
   });
 
   if (!fontsLoaded) {
     return StyleSheet.create({
       container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        justifyContent: "center",
+        alignItems: "center",
         backgroundColor: theme.primaryBackground,
       },
       text: {
@@ -44,14 +44,14 @@ export const AppStyles = () => {
   return StyleSheet.create({
     container: {
       // flex: 1,
-      alignItems: 'center',
+      alignItems: "center",
     },
     backgroundImage: {
       flex: 1,
-      justifyContent: 'center',
-      resizeMode: 'cover',
-      width: '100%',
-      height: '100%',
+      justifyContent: "center",
+      resizeMode: "cover",
+      width: "100%",
+      height: "100%",
     },
     text: {
       fontSize: 16,
@@ -65,8 +65,8 @@ export const AppStyles = () => {
       color: theme.primaryColor,
     },
     button: {
-      width: windowWidth*0.4,
-      height: windowHeight*0.05,
+      width: windowWidth * 0.4,
+      height: windowHeight * 0.05,
       borderRadius: 17,
       backgroundColor: theme.buttonColor,
       justifyContent: "center",
@@ -87,16 +87,16 @@ export const AppStyles = () => {
       marginBottom: 10,
     },
     bottleImage: {
-      marginTop: 15, 
-      width: windowWidth*0.7,
-      height: windowHeight*0.5,
+      marginTop: 15,
+      width: windowWidth * 0.7,
+      height: windowHeight * 0.5,
     },
     textInputBox: {
       borderRadius: 20,
       borderColor: theme.borderOutlineColor,
       backgroundColor: theme.white,
-      width: windowWidth*0.8,
-      height: windowHeight*0.05,
+      width: windowWidth * 0.8,
+      height: windowHeight * 0.05,
       margin: 12,
       borderWidth: 1,
       padding: 10,
@@ -122,10 +122,88 @@ export const AppStyles = () => {
       fontSize: 30,
     },
     shadowProp: {
-      shadowColor: '#171717',
-      shadowOffset: {width: -2, height: 4},
+      shadowColor: "#171717",
+      shadowOffset: { width: -2, height: 4 },
       shadowOpacity: 0.2,
       shadowRadius: 3,
+    },
+
+    // modal style
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22,
+    },
+    modalView: {
+      width: windowWidth * 0.8,
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    // button: {
+    //   borderRadius: 20,
+    //   padding: 10,
+    //   elevation: 2,
+    // },
+    buttonOpen: {
+      backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+      backgroundColor: "#2196F3",
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    modalText: {
+      color: theme.primaryColor,
+      textAlign: "center",
+      fontFamily: theme.titleFont,
+      fontSize: 16,
+      margin: 10,
+    },
+    modalRecieverImage: {
+      width: 100,
+      height: 100,
+      borderRadius: 60,
+      marginTop: 10,
+      marginBottom: 10,
+    },
+    modalRecieverName: {
+      color: theme.primaryColor,
+      textAlign: "center",
+      fontFamily: theme.textFont,
+      fontSize: 16,
+    },
+
+    // search bar
+    searchBarContainer: {
+      borderRadius: 20,
+      borderColor: theme.borderOutlineColor,
+      backgroundColor: theme.white,
+      width: windowWidth * 0.6,
+      height: windowHeight * 0.05,
+      borderWidth: 1,
+      padding: 10,
+      margin: 10,
+    },
+    closeIconContainer: {
+      position: "absolute",
+      top: 0,
+      right: 0,
+      padding: 10,
     },
   });
 };
