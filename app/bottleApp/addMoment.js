2530c2b4c3da3ceafb13ce48872e19aed1acbfb9
jduan22 <jenny.s.duan@gmail.com>
2023-12-02 13:26:44 -0800

    addMoment edits

--- app/bottleApp/addMoment.js
+++ app/bottleApp/addMoment.js
@@ -1,19 +1,30 @@
 import React from "react";
-import { Text, View, ImageBackground } from "react-native";
-import { Ionicons } from '@expo/vector-icons'; 
+import { Text, View, Image, ImageBackground } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
 import { AppStyles } from "../../utils/styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Link } from "expo-router";
 
 export default function addMoment() {
   const styles = AppStyles();
+  <Tabs
+      screenOptions={({ route }) => ({
+        tabBarStyle: {
+          display: route.name === 'example' ? 'none' : 'flex',
+        },
+      })}
+    >
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="(root)" options={{ href: null, }} />
+</Tabs>
   return (
     <ImageBackground
-          source={require("../../assets/background.png")}
-          opacity="0.5"
-          style={styles.backgroundImage}
-        >
-        <SafeAreaView style={styles.headerContainer}>
+      source={require("../../assets/background.png")}
+      opacity="0.5"
+      style={styles.backgroundImage}
+    >
+      <SafeAreaView>
+        <View style={styles.headerContainer}>
           <View style={styles.backIconContainer}>
             <Link href={{ pathname: 'bottleApp/home' }} style={{ marginRight: 8 }}>
               <Ionicons
@@ -23,15 +34,28 @@ export default function addMoment() {
               />
             </Link>
             <Text style={styles.subHeading}>My Bottle</Text>
-
           </View>
-        {/* <View style={styles.titleContainer}>
-          <Text style={styles.subHeading}>My Bottle</Text>
-        </View> */}
-
-
+        </View>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            borderRadius: 50,
+            borderWidth: 3,
+            borderColor: '#23AFBB'
+          }}
+          source={require('../../assets/people/selena.jpg')}
+        />
+        <Text style={styles.subHeading}>Selena</Text>
+        <Image
+          source={require("../../assets/graphics/bottle-cropped.png")}
+          style={styles.bottleImage}
+        />
+        <View style={styles.selectMedia}>
+          <Text>Hello world</Text>
+        </View>
       </SafeAreaView>
     </ImageBackground>
-    
+
   );
 }
