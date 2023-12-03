dfd990ce9aad39a51064d4ac525e16ba6eddabe9
LuciaLanganey <langaneylucia03@gmail.com>
2023-12-03 13:02:48 -0800

    archive page complete

--- app/bottleApp/archive.js
+++ app/bottleApp/archive.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, SafeAreaView } from 'react-native';
+import { AppStyles } from '../../utils/styles';
+import { Link } from 'expo-router';
 
 export default function Archive() {
+  const styles = AppStyles();
   return (
-    <View style={styles.container}>
-      <Text>Archive</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('../../assets/graphics/bottle_archive.png')}    
+      style={styles.backgroundImage}
+    >
+      <SafeAreaView>
+        <Link href={{ pathname: 'bottleApp/openBottle' }}>
+          <View style={{ height: 200, width: 100 }}>
+          </View>
+        </Link>
+        <View style={{ height: 200 }}>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
