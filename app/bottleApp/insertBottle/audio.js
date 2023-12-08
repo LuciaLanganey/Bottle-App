506f84a54865554422cc6e61c82752d021994ff3
evyshen <134675564+evyshen@users.noreply.github.com>
2023-12-07 22:01:12 -0800

    made changes to audio.js

--- app/bottleApp/insertBottle/audio.js
+++ app/bottleApp/insertBottle/audio.js
@@ -1,9 +1,11 @@
-import React, { useState, modalVisible } from "react";
-import { Text, View, ImageBackground, Image, TextInput, Pressable, Modal } from "react-native";
+import React, { useState } from "react";
+import { Text, View, ImageBackground, Dimensions, Image, TextInput, Pressable, Modal, StyleSheet } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import { AppStyles } from "../../../utils/styles";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Link } from "expo-router";
+
+const { height: windowHeight, width: windowWidth } = Dimensions.get("window");
 
 export default function openBottle() {
     const [text, setText] = useState(''); // State to keep track of text
@@ -49,6 +51,15 @@ export default function openBottle() {
                 <Image
                     source={require('../../../assets/graphics/EmptyBottle.png')} resizeMode={'contain'} style={styles.momentBottle}
                 />
+                {/* <Pressable
+                  style={[styles.button]}
+                  onPress={() => {
+                    setModalVisible(!modalVisible);
+                    // setSelectedRecipient(potentialRecipient);
+                  }}
+                >
+                  <Text style={styles.textStyle}>Done</Text>
+                </Pressable>
                 <Modal
             animationType="slide"
             transparent={true}
@@ -57,9 +68,9 @@ export default function openBottle() {
               Alert.alert("Modal has been closed.");
               setModalVisible(!modalVisible);
             }}
-          >
-                <View style={styles.popupAudioBox}>
-                    <View style={styles.audioBox} />
+          > */}
+                <View style={audiostyles.popupAudioBox}>
+                    <View style={audiostyles.audioBox} />
                     <View style>
                     <View style={{ position: 'absolute', left: 25, bottom: 60, borderRadius: 50, }}>
                             <Image source={require('../../../assets/moments/audioBlue.png')} resizeMode={'contain'} style = {styles.audioFile}
@@ -73,13 +84,57 @@ export default function openBottle() {
                         </View>
                         <View style={{padding: 10, position: 'absolute', left: '74%', bottom: 30, backgroundColor: 'white', borderRadius: 30 }}>
                             <Link href={{pathname: 'bottleApp/insertBottle/confirmation'}}>
-                                <Text style={{fontSize: 16, color: "#23AFBB", font: "Inter-Bold"}}>Next ></Text>
+                                <Text style={{fontSize: 16, color: "#23AFBB", font: "Inter-Bold"}}> Next </Text>
                             </Link>
                         </View>
                     </View>
                 </View>
-                </Modal>ins
+                {/* </Modal> */}
             </SafeAreaView>
         </ImageBackground>
     );
 }
+
+const audiostyles = StyleSheet.create ({
+    popupAudioBox: {
+        position: "absolute",
+        alignSelf: "center",
+        borderRadius: 20,
+        // borderColor: "red",
+        // borderWidth: 3,
+      },
+      deleteMessage: {
+        // position: 'absolute',
+        bottom: 0,
+        left: 0,
+        padding: 10,
+      },
+      audioBox: {
+        borderRadius: 20,
+        // borderColor: theme.borderOutlineColor,
+        // backgroundColor: theme.secondaryColor,
+        width: windowWidth * 0.8,
+        height: windowHeight * 0.17,
+        margin: 12,
+        borderWidth: 1,
+        padding: 20,
+        paddingTop: 20,
+        color: "white",
+        fontSize: 20,
+        // font: "Inter-Regular",
+      },
+      audioFile: {
+        resizeMode: "contain",
+        alignItems: "center",
+        maxHeight: 100,
+        maxWidth: 300,
+      },
+      audioContainer: {
+        flex: 1,
+        justifyContent: "flex-start",
+        width: "80%",
+        resizeMode: "contain",
+      },
+        marginTop: '120%',
+      
+})
